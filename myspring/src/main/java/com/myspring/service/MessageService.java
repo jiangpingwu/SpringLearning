44bc6497fb54ad96fb3c7687d8ca835e3f6c9960
alan <252842510@qq.com>
2019-08-12 19:03:58 +0800

    add

---
+++ myspring/src/main/java/com/myspring/service/MessageService.java
@@ -0,0 +1,13 @@
+package com.myspring.service;
+
+/**
+ * @ClassName: MessageService
+ * @description: TODO
+ * @author: alan
+ * @date: 2019-08-09 20:09
+ * @version: V1.0
+ **/
+public interface MessageService {
+
+    String getMessage();
+}
---
+++ myspring/src/main/java/com/myspring/service/impl/MessageServiceImpl.java
@@ -0,0 +1,17 @@
+package com.myspring.service.impl;
+
+import com.myspring.service.MessageService;
+
+/**
+ * @ClassName: MessageServiceImpl
+ * @description: TODO
+ * @author: alan
+ * @date: 2019-08-09 20:10
+ * @version: V1.0
+ **/
+public class MessageServiceImpl implements MessageService {
+
+    public String getMessage() {
+        return "hello world";
+    }
+}
